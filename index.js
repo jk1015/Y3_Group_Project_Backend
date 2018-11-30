47dab37e3c0aaee468b7fd91018c14f7ae8e7c1a
Max Smith <mws15@ic.ac.uk>
2018-11-30 16:22:07 +0000

    added server stuff

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ app.use('/api', router);
 
 server.listen(port);
 
+app.all('/', function(req, res, next) {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "X-Requested-With");
+ next();
+});
+
 app.get('/', (req, res) => {
     res.send('Backend');
 });
