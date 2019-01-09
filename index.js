be2ea94afee4e7195f41bb9215320069c82d8daa
ChangLiu0106 <cl5618@ic.ac.uk>
2019-01-09 17:31:07 +0000

    visualization

--- index.js
+++ index.js
@@ -16,10 +16,13 @@ const auth = require('basic-auth');
 const ldapHandler = require('./ldapHandler');
 const CryptoJS = require("crypto-js");
 const SECRET_KEY = "EwWtEuAwByns9uALoH3zUdY5FoTAQzeq";
+const bodyParser = require('body-parser');
 
 app.use(cors());
 
 require('./routes')(router);
+
+app.use(bodyParser.json());
 
 app.use('/api', router);
 
@@ -33,6 +36,17 @@ server.listen(port);
 
 app.get('/', (req, res) => {
     res.send('Backend');
+});
+
+app.get('/data', (req, res) => {
+
+  questionHandler.getAllData()
+  .then(result =>{
+    res.status(200).json(result);
+  })
+
+  .catch(err => res.status(err.status).json(err.message));
+
 });
 
 app.post('/data/:room', (req, res) => {
@@ -55,7 +69,8 @@ console.log(`App Runs on ${port}`);
 
 // console.log(checkCourseSlot('382', startDate, endDate));
 
-let questionMaps = new HashMap();
+let studentQuestionMaps = new HashMap();
+let lecturerQuestionMaps = new HashMap();
 
 
 io.on('connection', (socket) => {
@@ -93,10 +108,13 @@ io.on('connection', (socket) => {
           //Check if the user has student or lecturer access
           if(userType == res.doc_user){
             //Check if user have access to this course
-            if(res.courses.indexOf(room) > -1){
+            if(true || res.courses.indexOf(room) > -1){
               socket.join(room);
-              if (!questionMaps.size || !questionMaps.has(room)) {
-                  questionMaps.set(room, new HashMap());
+              if (!studentQuestionMaps.size || !studentQuestionMaps.has(room)) {
+                  studentQuestionMaps.set(room, new HashMap());
+              }
+              if (!lecturerQuestionMaps.size || !lecturerQuestionMaps.has(room)) {
+                  lecturerQuestionMaps.set(room, new HashMap());
               }
               let rooms = Object.keys(socket.rooms);
               socket.emit('on rooms lists', rooms);
@@ -125,50 +143,81 @@ io.on('connection', (socket) => {
         socket.emit('on rooms lists', rooms);
     });
 
-    socket.on('question asked', (message, user) => {
+    socket.on('question asked', (message, data) => {
 
-      questionHandler.create(message, user.room)
-      .then((id) => {
+      if(data.user.type == "student") {
+        questionHandler.create(message, data.user.room)
+        .then((id) => {
 
-        let questionMap = questionMaps.get(user.room);
+          let studentQuestionMap = studentQuestionMaps.get(data.user.room);
 
-        if (questionMap.has(message)) {
-            let question = questionMap.get(message);
-            question.users.push({name: user.name, login: user.login, question_id: id});
-            question.count = question.count + 1;
-            questionMap.set(message, question);
-        } else {
-            questionMap.set(message,
-                {users: [{name: user.name, login: user.login, question_id: id}], count: 1});
+          if (studentQuestionMap.has(message)) {
+              let question = studentQuestionMap.get(message);
+              question.users.push({name: data.user.name, login: data.user.login, question_id: id});
+              question.count = question.count + 1;
+              studentQuestionMap.set(message, question);
+          } else {
+              studentQuestionMap.set(message,
+                  {users: [{name: data.user.name, login: data.user.login, question_id: id}], count: 1});
+          }
+
+          io.in(data.user.room).emit('question received', {
+              question: message,
+              data: studentQuestionMap.get(message),
+              question_id: id,
+              user: data.user.login,
+              type: "student"
+          });
+        })
+        .catch((err) => {
+            // io.in(data.user.room).emit('question received', { question: question,
+            //   data: questionMap.get(question)});
+        })
+      }
+      else if(data.user.type == "lecturer") {
+
+        let lecturerQuestionMap = lecturerQuestionMaps.get(data.user.room);
+
+        //Add to database
+        let id = 0;
+        //Add to lecturer question map
+        if (!lecturerQuestionMap.has(message)) {
+          lecturerQuestionMap.set(message,
+            {answers: [],
+             type: data.question_type,
+             options: data.options,
+             count: 0});
         }
 
-        io.in(user.room).emit('question received', {
+        //Emit into room
+        io.in(data.user.room).emit('question received', {
             question: message,
-            data: questionMap.get(message), question_id: id, user: user.login
+            data: lecturerQuestionMap.get(message),
+            question_id: id,
+            user: data.user.login,
+            type: data.question_type,
+            options: data.options
         });
-      })
-      .catch((err) => {
-          // io.in(user.room).emit('question received', { question: message,
-          //   data: questionMap.get(message)});
-      })
+      }
 
     });
 
     socket.on('lecturer connect', room => {
         //let room = Object.keys(socket.rooms)[1];
-        let questionMap = questionMaps.get(room);
+        let studentQuestionMap = studentQuestionMaps.get(room);
+        let lecturerQuestionMap = lecturerQuestionMaps.get(room);
         // console.log("Question map after lecturer connect: " + questionMaps.get(room).entries());
-        socket.emit('on lecturer connect', questionMap);
+        socket.emit('on lecturer connect', {sqm: studentQuestionMap, lqm: lecturerQuestionMap});
     });
 
-    socket.on('answer question', (question, room) => {
-        let questionMap = questionMaps.get(room);
-        let question_ids = questionMap.get(question).users.map(x => x.question_id.id);
-        //console.log(question_ids);
-        questionHandler.stopAskingBatch(question_ids, "Lecturer answered question")
+    socket.on('answer student question', (question, room) => {
+        let studentQuestionMap = studentQuestionMaps.get(room);
+        let question_ids = studentQuestionMap.get(question).users.map(x => x.question_id.id);
+        console.log(question_ids);
+        questionHandler.stopAskingBatch(question_ids, "answered question")
         .then(id => {
-            questionMap.delete(question);
-            io.in(room).emit('question answered', question);
+            studentQuestionMap.delete(question);
+            io.in(room).emit('student question answered', question);
         })
         .catch(err => {
             // io.in(user.room).emit('question received', { question: message,
@@ -176,15 +225,24 @@ io.on('connection', (socket) => {
         });
     });
 
+    socket.on('answer lecturer question', (question, answer, room) => {
+        let lecturerQuestionMap = lecturerQuestionMaps.get(room);
+        lecturerQuestionMap.get(question).answers.push(answer);
+        (lecturerQuestionMap.get(question).count)++;
+        io.in(room).emit('lecturer question answered', {question: question, answer: answer})
+    });
+
     socket.on('clear all', room => {
-        let questionMap = questionMaps.get(room);
-        let question_ids = [].concat(...questionMap.values().map(x =>
+        let lecturerQuestionMap = lecturerQuestionMaps.get(room);
+        let studentQuestionMap = studentQuestionMaps.get(room);
+        let question_ids = [].concat(...studentQuestionMap.values().map(x =>
             x.users.map(y => y.question_id.id)
         ));
         //console.log(question_ids);
         questionHandler.stopAskingBatch(question_ids, "Lecturer cleared question")
         .then(id => {
-            questionMap.clear();
+            lecturerQuestionMap.clear();
+            studentQuestionMap.clear();
             io.in(room).emit('on clear all');
         })
         .catch(err => {
@@ -198,21 +256,21 @@ io.on('connection', (socket) => {
         //console.log(user.question_id);
         questionHandler.stopAsking(user.question_id, "Student withdrew question")
         .then(id => {
-            let questionMap = questionMaps.get(user.room);
+            let studentQuestionMap = studentQuestionMaps.get(user.room);
 
-            if (questionMap.has(message)) {
-                let question = questionMap.get(message);
+            if (studentQuestionMap.has(message)) {
+                let question = studentQuestionMap.get(message);
 
                 if (--question.count <= 0) {
-                    questionMap.delete(message);
+                    studentQuestionMap.delete(message);
                 } else {
                     question.users = question.users.filter(x => x.name !== user.name);
-                    questionMap.set(message, question);
+                    studentQuestionMap.set(message, question);
                 }
             }
             io.in(user.room).emit('question received', {
                 question: message,
-                data: questionMap.get(message) ? questionMap.get(message) : null
+                data: studentQuestionMap.get(message) ? studentQuestionMap.get(message) : null
             });
         })
         .catch((err) => {
